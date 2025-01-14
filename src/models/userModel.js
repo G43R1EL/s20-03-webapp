d2117b3c6ebafbb83974c53c1d4e6ca319780e29
Nelson Gabriel Hernández <gabrielymarg@gmail.com>
2025-01-14 00:53:45 -0300

    -Login function added to authController -Auth Route updated to support authController -JWT Expiration added to Config file -jsonwebtoken dependency added to project -Responses file updated -userSchema improvements: added custom method to match password, bcrypt changed to async for performance -Improved code comments

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,5 @@
-const config = require('../config');
 const mongoose = require('mongoose');
 const userSchema = require('../schemas/userSchema');
-const bcrypt = require('bcrypt');
 
 // Define the user model
 const userModel = mongoose.model('User', userSchema);
